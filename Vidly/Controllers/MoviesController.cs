dda4de2b2ae0be305291c71640a7042ad4c1300c
Bobby Speirs <bobby.speirs@gmail.com>
2017-05-17 12:01:12 -0400

    Added client-side validation and anti-forgery token

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -70,6 +70,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
             if (!ModelState.IsValid)
